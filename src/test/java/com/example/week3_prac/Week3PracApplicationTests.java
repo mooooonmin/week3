481e5b9b276101e5375441573df20b293c8e3291
min_moon <minhee610@gmail.com>
2023-10-29 16:06:06 +0900

    init commit

---
+++ src/test/java/com/example/week3_prac/Week3PracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week3_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3PracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
